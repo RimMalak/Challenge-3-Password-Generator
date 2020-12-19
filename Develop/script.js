eb414e3595b773ef9fc4f1d24c455d4c15059d2f
Perlatum <rimmalak@hotmail.com>
2020-12-18 21:27:27 -0500

    prompt functions created

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,43 @@
 // Assignment code here
 
+var psswdLength = function()
+{
+  pLength = window.prompt("Please select the length of your password, it should have a MIN of 8 characters and a MAX of 128.");
+  pLength = parseInt(pLength);
+
+  if (pLength < 8 || pLength > 128)
+  {
+    pLength = window.prompt("Invalid length! Please select the length of your password, it should have a MIN of 8 and a MAX of 128 characters.");
+    psswdLength();
+  }
+  return pLength;
+};
+
+var psswdCharType = function()
+{
+  lowCase = window.confirm("Do you want to include LOWERCASE characters? Please click 'OK' for yes or cancel for 'NO'");
+  upCase = window.confirm("Do you want to include UPPERCASE characters? Please click 'OK' for yes or cancel for 'NO'");
+  numChar = window.confirm("Do you want to include NUMERIC characters? Please click 'OK' for yes or cancel for 'NO'");
+  spChar = window.confirm("Do you want to include SPECIAL characters? Please click 'OK' for yes or cancel for 'NO'");
+
+  if (lowCase != true && upCase != true && numChar != true && spChar != true )
+  {
+    window.alert("Please choose at least ONE of these options");
+    psswdCharType();
+  }
+};
+
+var concatDisplay = function()
+{
+
+}
+
+
+
+//var type = [lowerCase, upperCase, numeric, special];
+
+//parseInt(length) = window.alert("Please select the length of your password, it should have a MIN of 8 characters and a MAX of 128.");
+//type = window.alert("Please select at LEAST ONE of the character types to include in your password: 1 LOWER-CASE, ");
 
 
 
@@ -17,3 +55,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+psswdLength();
+psswdCharType();
