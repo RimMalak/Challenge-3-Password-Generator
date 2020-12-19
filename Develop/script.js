b41060957352a7c696505ad21759502402a0add1
Perlatum <rimmalak@hotmail.com>
2020-12-19 01:58:37 -0500

    btn and display

--- Develop/script.js
+++ Develop/script.js
@@ -4,34 +4,42 @@ var lowCase;
 var upCase;
 var numChar;
 var spChar;
+var psswdRand;
 
 var psswdLength = function()
 {
   pLength = window.prompt("Please select the length of your password, it should have a MIN of 8 characters and a MAX of 128.");
-  pLength = parseInt(pLength);
-
-  if (pLength < 8 || pLength > 128)
+  if (pLength)
   {
-    pLength = window.prompt("Invalid length! Please select the length of your password, it should have a MIN of 8 and a MAX of 128 characters.");
-    psswdLength();
-  }
-};
+    pLength = parseInt(pLength);
 
-var psswdCharType = function()
-{
-  lowCase = window.confirm("Do you want to include LOWERCASE characters? Please click 'OK' for yes or cancel for 'NO'");
-  upCase = window.confirm("Do you want to include UPPERCASE characters? Please click 'OK' for yes or cancel for 'NO'");
-  numChar = window.confirm("Do you want to include NUMERIC characters? Please click 'OK' for yes or cancel for 'NO'");
-  spChar = window.confirm("Do you want to include SPECIAL characters? Please click 'OK' for yes or cancel for 'NO'");
-
-  if (lowCase != true && upCase != true && numChar != true && spChar != true )
-  {
-    window.alert("Please choose at least ONE of these options");
+    if (pLength < 8 || pLength > 128)
+      {
+        pLength = window.prompt("Invalid length! Please select the length of your password, it should have a MIN of 8 and a MAX of 128 characters.");
+        psswdLength();
+      }
     psswdCharType();
   }
+  else
+  {
+    window.alert("Goodbye!")
+  }
 };
 
+  var psswdCharType = function()
+  {
+    lowCase = window.confirm("Do you want to include LOWERCASE characters? Please click 'OK' for yes or cancel for 'NO'");
+    upCase = window.confirm("Do you want to include UPPERCASE characters? Please click 'OK' for yes or cancel for 'NO'");
+    numChar = window.confirm("Do you want to include NUMERIC characters? Please click 'OK' for yes or cancel for 'NO'");
+    spChar = window.confirm("Do you want to include SPECIAL characters? Please click 'OK' for yes or cancel for 'NO'");
 
+    if (lowCase != true && upCase != true && numChar != true && spChar != true )
+    {
+      window.alert("Please choose at least ONE of these options");
+      psswdCharType();
+    }
+    writePassword()
+  };
 
 var pGenerator = function()
 {
@@ -40,19 +48,18 @@ var pGenerator = function()
   var upChar  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var numVal  = '0123456789';
   var spVal   = '!"&()*+,%-./$:;<=>#?@[\]^_`{|}~';
+  var i = 0;
   
-  for (var i = 0; i < pLength; i++)
-  
+  while (i < pLength)
   {
-    debugger;
     if (lowCase && i < pLength)
     {
-      psswdRand += lowChar.charAt(Math.floor(Math.random()*10 + 16))
+      psswdRand += lowChar.charAt(Math.floor(Math.random() * Math.floor(26)));
       i++;
     }
-    if(upChar && i < pLength)
+    if(upCase && i < pLength)
     {
-      psswdRand += upChar.charAt(Math.floor(Math.random()*10 + 16));
+      psswdRand += upChar.charAt(Math.floor(Math.random() * Math.floor(26)));
       i++;
     }
     if (numChar && i < pLength)
@@ -62,10 +69,12 @@ var pGenerator = function()
     }
     if (spChar && i < pLength)
     {
-      psswdRand += spVal.charAt(Math.floor(Math.random()*10 + 21));
+      psswdRand += spVal.charAt(Math.floor(Math.random() * Math.floor(31)));
+      i++;
     }
   }
-  console.log(psswdRand);
+  window.alert("Your password is : " + psswdRand);
+  return psswdRand;
 };
 
 // Get references to the #generate element
@@ -73,16 +82,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password =  pGenerator();//generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.value =  password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", psswdLength);
 
-psswdLength();
-psswdCharType();
-pGenerator();
+
+
+
+
+
